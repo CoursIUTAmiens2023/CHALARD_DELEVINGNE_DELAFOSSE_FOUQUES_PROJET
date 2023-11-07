de4665dbead6494f46ccb7689e36417492f7db90
ChalN12 <nathanchalard1@gmail.com>
2023-11-07 19:48:33 +0100

    changement pacman

--- pacman.py
+++ pacman.py
@@ -1,4 +1,6 @@
 import pygame
+from food import Food
+from map import Map
 
 
 class Pacman:
@@ -9,6 +11,14 @@ class Pacman:
       # Autres attributs et méthodes pour le mouvement, la direction, etc.
   
   def move(self, dx, dy):
+    #vérification des murs
+    #faudrait rendre plato public pour y avoir acces dans les move je pense
+    if Map.plateau[dx,dy] != 0:
+      #Vérification si une pacgomme se trouve sur la prochaine case
+      pacgomme = Food(dx, dy)
+      pacgomme.eat(Map.plateau[dx,dy])
+      #puis on incrémente la position de pacman 
       self.x += dx
       self.y += dy
-      return 0
+    
+    return 0
